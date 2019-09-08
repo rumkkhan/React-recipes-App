7ce83b6345314ab8637302538f18d50fef97840f
RSL <mohd.ramzan@relyonsoft.com>
2019-09-08 23:08:42 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <div>
+          HI, it is my first page
+    </div>
+  );
+}
+
+export default App;
